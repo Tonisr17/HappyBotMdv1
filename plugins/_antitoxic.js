cb6c3d401b53db60a8191047911510828be6af3a
Tonisr17 <122715688+Tonisr17@users.noreply.github.com>
2023-02-11 05:39:17 +0700

    Add files via upload

---
+++ plugins/_antitoxic.js
@@ -0,0 +1,30 @@
+
+const isToxic = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|babi|anj|bangsad|bgsd|peler|pantek|ngentod|kontol|ngentd|ngntod|koncol|kncl|kncol|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|a(su|sw|syu)/i // tambahin sendiri
+
+export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = global.db.data.chats[m.chat]
+    let name = conn.getName(m.sender)
+    let bot = global.db.data.settings[this.user.jid] || {}
+    const isAntiToxic = isToxic.exec(m.text)
+    let hapus = m.key.participant
+    let bang = m.key.id
+    
+    if (chat.antiToxic && isAntiToxic) {
+        await conn.sendButton(m.chat, `n╭─────═[ ANTI TOXIC ]═─────⋆\n┊   ╭───────────────···\n┊   │☂︎ *Terdekteksi ${name}Telah mengirim Kata-Kata Aneh!
+📛 ${name}
+🔴 ${ucapan}
+📆 Tangal ${date} ╰────────────────···\n╰──────────═┅═──────────\n\      ` ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['Owner', '/owner kontak'], m)
+        if (isBotAdmin && bot.restrict) {
+            // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+ /*   global.db.data.users[m.sender].warn += 1
+    global.db.data.users[m.sender].banned = true*/
+    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+        } else if (!bot.restrict) return m.reply('Semoga harimu suram!')
+    }
+    return !0
+}
+
+export const disable = true
